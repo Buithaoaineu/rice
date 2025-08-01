82933976dbc9640ad80a06114a58094736ddae65
Buithaoaineu <buithao080106@gmail.com>
2025-08-01 16:27:27 +0700

    Upload CV

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-print("1. Borrow book")
-print("2. Return book")
-print("3. View book")
+class Student :
+    def __init__ (self, student_ID, name, birth_date, class_name) :
+        self.student_ID = student_ID
+        self.name = name
+        self.birth_date = birth_date
+        self.class_name = class_name
+    
+    def __str__(self) :
+        return f"""
+    Student ID : {self.student_ID}
+    Student name : {self.name}
+    Birth date : {self.birth_date}
+    Class_name : {self.class_name}"""
 
-choice = input ("Choose a number :")
-
-if choice ==1 :
-    print ("Book is not available to borrow")
-
-elif choice == 2 : 
-    print ("Book is successfully returned")
-
-elif choice == 3 :
-    print ("You're viewing a book :D")
+student_1 = Student("11247353", "Bùi Phương Thảo", "08/01/2006", "AI66B")
+print(student_1)
 
